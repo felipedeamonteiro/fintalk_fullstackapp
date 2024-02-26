7030087b5f7813ff599f862e6f870aacce15c08c
Felipe Monteiro <felipedeamonteiro@gmail.com>
2024-02-26 17:09:50 -0300

    Add edit server modal and its backend route

--- app/api/servers/[serverId]/invite-code/route.ts
+++ app/api/servers/[serverId]/invite-code/route.ts
@@ -1,17 +1,21 @@
+import { v4 as uuidv4 } from 'uuid';
 import {currentProfile} from '@/lib/current-profile';
 import {db} from '@/lib/db';
 import {NextResponse} from 'next/server';
 
-export async function PATCH(
+export async function PATCH (
   req: Request,
   { params }: { params: { serverId: string } }
 ) {
   try {
     const profile = await currentProfile();
-    const { name, imageUrl } = await req.json();
 
-    if(!profile) {
-      return new NextResponse('Unauthorized', { status: 401 });
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!params.serverId) {
+      return new NextResponse("Server ID Missing", { status: 400 });
     }
 
     const server = await db.server.update({
@@ -20,14 +24,13 @@ export async function PATCH(
         profileId: profile.id,
       },
       data: {
-        name,
-        imageUrl,
+        inviteCode: uuidv4()
       }
     });
 
     return NextResponse.json(server);
   } catch (error) {
-    console.log('[SERVER_ID_PATCH]', error);
-    return new NextResponse('Internal Error', { status: 500 })
+    console.log("[SERVER_ID]", error);
+    return new NextResponse("Internal Error", { status: 500 });
   }
 }
