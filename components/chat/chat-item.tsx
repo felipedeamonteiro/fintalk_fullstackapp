920082477a1b41df207133826b5b53489822bff0
Felipe Monteiro <felipedeamonteiro@gmail.com>
2024-03-06 02:43:16 -0300

    Add a fix to possible error with websockets

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -5,6 +5,7 @@ import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import axios from 'axios';
 import qs from 'query-string';
+import { useRouter, useParams } from 'next/navigation';
 
 import { Member, MemberRole, Profile } from '@prisma/client';
 import { UserAvatar } from '@/components/user-avatar';
@@ -57,6 +58,16 @@ export const ChatItem = ({
 }: ChatItemProps) => {
   const [isEditing, setIsEditing] = useState(false);
   const { onOpen } = useModal();
+  const router = useRouter();
+  const params = useParams();
+
+  const onMemberClick = () => {
+    if (member.id === currentMember.id) {
+      return;
+    }
+
+    router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+  };
 
   useEffect(() => {
     const handleKeyDown = (event: any) => {
@@ -114,13 +125,19 @@ export const ChatItem = ({
   return (
     <div className='relative group flex items-center hover:bg-black/5 p-4 transition w-full'>
       <div className='group flex gap-x-2 items-start w-full'>
-        <div className='cursor-pointer horver:drop-shadow-md transition'>
+        <div
+          onClick={onMemberClick}
+          className='cursor-pointer horver:drop-shadow-md transition'
+        >
           <UserAvatar src={member.profile.imageUrl} />
         </div>
         <div className='flex flex-col w-full'>
           <div className='flex items-center gap-x-2'>
             <div className='flex items-center'>
-              <p className='font-semibold text-sm hover:underline cursor-pointer'>
+              <p
+                onClick={onMemberClick}
+                className='font-semibold text-sm hover:underline cursor-pointer'
+              >
                 {member.profile.name}
               </p>
               <ActionTooltip label={member.role}>
